00492efce333def6c276ec142493f5cfd353a535
facu <facu@Facu-Unix>
2015-11-25 15:43:09 -0300

    asd

---
+++ SFC Final/src/SFC Final.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : SFC.cpp
+// Author      : FME
+// Version     :
+// Copyright   : 2do Cuatrimestre 2015
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "SFC" << endl; // prints SFC
+	return 0;
+}
