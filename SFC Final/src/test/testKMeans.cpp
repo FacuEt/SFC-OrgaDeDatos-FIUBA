b241f6809931d69a27aad243486a2c7e91833046
tbert <tomasbert@hotmail.com>
2015-11-27 00:32:01 -0300

    KMEANS igual que python!

--- SFC Final/src/test/testKMeans.cpp
+++ SFC Final/src/test/testKMeans.cpp
@@ -1,7 +1,23 @@
 #include "test.h"
 
 void testKMeans(){
-	//Punto* p1
-	//vector<Punto*> puntos {p1,p2,p3,p4,p5};
+	Punto* p1 = new Punto{1,5};
+	Punto* p2 = new Punto{3,2};
+	Punto* p3 = new Punto{4,6};
+	Punto* p4 = new Punto{10,5};
+	Punto* p5 = new Punto{7,3};
+
+	vector<Punto*> puntos {p1,p2,p3,p4,p5};
+
+	KMeans* clf = new KMeans(2);
+
+	clf->fit(puntos);
+
+	clf->viewCentroides();
+
+	for (size_t i = 0; i < puntos.size(); i++){
+		cout << "P: (" << puntos[i]->x << "," << puntos[i]->y << ") \t ->";
+		cout << clf->predict(puntos[i]) << endl;
+	}
 
 }
